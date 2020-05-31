5239cb04b382c6bd036a5845f540b6a828b6fbb9
shuma1127 <shuma.1127b@gmail.com>
2020-05-31 17:44:58 +0900

    hair(view)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  root to: 'homepage#index'
+  resources :homepage, only: [:index] do
+  end
 end
---
+++ test/controllers/hair_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HairControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
